5a13b58d8d27abf8a9fbd37e94d2ddb42e28932b
revel111 <s28324@pjwstk.edu.pl>
2024-06-25 23:01:54 +0200

    Initial commit

---
+++ src/main/java/com/example/quizzzin/QuizzzinApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizzzin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzzinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzzinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizzzin/QuizzzinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizzzin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzzinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
